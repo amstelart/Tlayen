6678d97c9661179b79fcccbae4bbca3cc9d82a3f
amstelart <amsteldroid@gmail.com>
2023-02-20 09:22:03 +0300

    02 commit

--- src/blocks/owl-carousel/owl-carousel.js
+++ src/blocks/owl-carousel/owl-carousel.js
@@ -1,33 +1,11 @@
-// $(document).ready(function(){
+$(document).ready(function(){
 
-//   $("#owl-carousel-demo").owlCarousel({
-//     items: 3,
-//     nav: true,
-//     loop: true,
-//     center: true,
-//     responsive : {
-//       0 : {
-//         items: 1,
-//         nav: true,
-//         loop: true,
-//         center: true,
-//       },
-//       480 : {
-//         items: 3,
-//       },
-//       768 : {
-//         items: 3,
-//       },
-//       992 : {
-//         items: 3,
-//       },
-//       1200 : {
-//         items: 3,
-//       },
-//       1800 : {
-//         items: 3,
-//       }
-//     }
-//   });
+  $("#owl-carousel-demo").owlCarousel({
+    items: 1,
+    nav: true,
+    margin: 40,
+    loop: false,
+    center: true
+  });
 
-// });
+});
